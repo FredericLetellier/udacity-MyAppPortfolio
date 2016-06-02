3e1800f8b72418b32041af684118bf868af2874e
Frédéric Letellier <frederic.letellier.pro@gmail.com>
2016-06-02 10:48:39 +0200

    First submission

--- app/src/main/java/technology/tasty/myappportfolio/MainActivity.java
+++ app/src/main/java/technology/tasty/myappportfolio/MainActivity.java
@@ -1,7 +1,10 @@
 package technology.tasty.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user touches the button */
+    public void sendMessage1(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Popular movies app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage2(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Stock Hawk app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage3(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Build it Bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage4(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Make Your App Material app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage5(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Go Ubiquitous app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage6(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
 }
